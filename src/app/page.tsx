a4b07009331e69f1c1b38e31c4b4c0b80ed3594a
YASH GHARAT <53621256+Yash8452@users.noreply.github.com>
2024-03-14 07:25:38 +0000

    clean up

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between pt-2 p-24">
+      <h1 className="text-3xl fond-extrabold">DASHBOARD</h1>
+    </main>
+  );
+}
